22df2fe55db250919ff14fe5718b43da8c942696
TonyJr03 <oscar031027@gmail.com>
2026-04-29 21:31:33 -0400

    refactor: eliminar archivos de datos de ejemplo y contenido demo

--- data/index.ts
+++
@@ -1,9 +0,0 @@
-export { highlightItems } from './highlights';
-export { aboutContent } from './about-content';
-export { blogPosts } from './blog-posts';
-export { faqItems } from './faq';
-export { galleryItems } from './gallery';
-export { categories } from './categories';
-export { products } from './products';
-export { promotions } from './promotions';
-export { testimonials } from './testimonials';
